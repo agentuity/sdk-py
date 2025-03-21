4b57e3db445eade7e7bcd78e9c0de0c78a186f67
Jeff Haynie <jhaynie@gmail.com>
2025-03-20 21:28:42 -0500

    Fix issue where other non-AI completion urls are trying to use the api key

--- agentuity/instrument/httpx_wrap.py
+++ agentuity/instrument/httpx_wrap.py
@@ -1,6 +1,12 @@
 import os
 import httpx
 import wrapt
+from agentuity import __version__
+
+gateway_urls = [
+    "https://api.agentuity.com/sdk/gateway",
+    "https://agentuity.ai/sdk/gateway",
+]
 
 
 def instrument():
@@ -9,6 +15,9 @@ def instrument():
     @wrapt.patch_function_wrapper(httpx.Client, "send")
     def wrapped_request(wrapped, instance, args, kwargs):
         request = args[0] if args else kwargs.get("request")
-        agentuity_api_key = os.getenv("AGENTUITY_API_KEY", None)
-        request.headers["Authorization"] = f"Bearer {agentuity_api_key}"
+        url = str(request.url)
+        if any(gateway_url in url for gateway_url in gateway_urls):
+            agentuity_api_key = os.getenv("AGENTUITY_API_KEY", None)
+            request.headers["Authorization"] = f"Bearer {agentuity_api_key}"
+            request.headers["User-Agent"] = f"Agentuity Python SDK/{__version__}"
         return wrapped(*args, **kwargs)
